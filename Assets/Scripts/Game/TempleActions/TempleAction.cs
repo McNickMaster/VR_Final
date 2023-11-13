b6c1c33d9081ea0b0ee4957bce957b264d22ad96
Nick McMaster <mcmasternick5572@gmail.com>
2023-11-13 13:59:25 -0500

    Auto stash before merge of "main" and "origin/main"

---
+++ Assets/Scripts/Game/TempleActions/TempleAction.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class TempleAction : MonoBehaviour
+{
+    public abstract void Start_Action();
+}
